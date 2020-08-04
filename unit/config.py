7b45a5a8f28bf02ed6042717b5660f7c728cbc41
GodWriter <18917565247@163.com>
2020-08-04 11:27:16 +0800

    Commit unit.

--- unit/config.py
+++ unit/config.py
@@ -10,7 +10,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument('--epochs', type=int, default=100, help="training epochs")
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    parser.add_argument('--batch_size', type=int, default=64, help='batch size')
     parser.add_argument('--dataset', type=str, default='comic2human', help='name of the dataset')
     parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
     parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
@@ -28,7 +28,7 @@ def parse_args():
     parser.add_argument('--n_downsample', type=int, default=2, help='number downsampling layers in encoder')
     parser.add_argument('--n_upsample', type=int, default=2, help='number sampling layers in decoder')
     parser.add_argument('--dim', type=int, default=64, help='number of filters in first encoder layer')
-    parser.add_argument('--sample_interval', type=int, default=200, help='interval between image samples')
+    parser.add_argument('--sample_interval', type=int, default=100, help='interval between image samples')
     parser.add_argument('--checkpoint_interval', type=int, default=5000, help='interval between saving models')
     parser.add_argument('--load_model', type=str, default='checkpoints/E1_done.pth,checkpoints/E2_done.pth', help='model to load')
     parser.add_argument('--test_img', type=str, default='images/test.jpg', help='image to test')
