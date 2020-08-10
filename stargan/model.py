855165e57a93e70198ed88b7774c874b4d4cdc53
GodWriter <18917565247@163.com>
2020-08-10 11:53:32 +0800

    Commit StarGAN.

--- stargan/model.py
+++ stargan/model.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 import torch.nn as nn
 
 from torch.autograd import Variable
@@ -15,20 +14,6 @@ def weights_init_normal(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
         torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(m.bias.data, 0.0)
-
-
-class LambdaLR:
-    def __init__(self, n_epochs, offset, decay_start_epoch):
-        assert (n_epochs - decay_start_epoch) > 0, "Decay must start before the training session ends"
-        self.n_epochs = n_epochs
-        self.offset = offset
-        self.decay_start_epoch = decay_start_epoch
-
-    def step(self, epoch):
-        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch) / (self.n_epochs - self.decay_start_epoch)
 
 
 class ResidualBlock(nn.Module):
@@ -36,106 +21,82 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
 
         self.block = nn.Sequential(nn.ReflectionPad2d(1),
-                                   nn.Conv2d(in_channels, in_channels, 3),
-                                   nn.InstanceNorm2d(in_channels),
+                                   nn.Conv2d(in_channels, in_channels, 3, bias=False),
+                                   nn.InstanceNorm2d(in_channels, affine=True, track_running_stats=True),
                                    nn.ReLU(inplace=True),
                                    nn.ReflectionPad2d(1),
-                                   nn.Conv2d(in_channels, in_channels, 3),
-                                   nn.InstanceNorm2d(in_channels))
+                                   nn.Conv2d(in_channels, in_channels, 3, bias=False),
+                                   nn.InstanceNorm2d(in_channels, affine=True, track_running_stats=True))
 
     def forward(self, x):
         return x + self.block(x)
 
 
-class Encoder(nn.Module):
-    def __init__(self, in_channels=3, dim=64, n_downsample=2, shared_block=None):
-        super(Encoder, self).__init__()
+class Generator(nn.Module):
+    def __init__(self, channels=3, res_blocks=9, c_dim=5):
+        super(Generator, self).__init__()
 
         # Initial convolution block
-        layers = [nn.ReflectionPad2d(3),
-                  nn.Conv2d(in_channels, dim, 7),
-                  nn.InstanceNorm2d(64),
-                  nn.LeakyReLU(0.2, inplace=True)]
+        model = [nn.Conv2d(channels + c_dim, 64, 7, stride=1, padding=3, bias=False),
+                 nn.InstanceNorm2d(64, affine=True, track_running_stats=True),
+                 nn.ReLU(inplace=True)]
 
         # Downsampling
-        for _ in range(n_downsample):
-            layers += [nn.Conv2d(dim, dim*2, 4, stride=2, padding=1),
-                       nn.InstanceNorm2d(dim * 2),
-                       nn.ReLU(inplace=True)]
+        dim = 64
+        for _ in range(2):
+            model += [nn.Conv2d(dim, dim * 2, 4, stride=2, padding=1, bias=False),
+                      nn.InstanceNorm2d(dim * 2, affine=True, track_running_stats=True),
+                      nn.ReLU(inplace=True)]
             dim *= 2
 
         # Residual blocks
-        for _ in range(3):
-            layers += [ResidualBlock(dim)]
+        for _ in range(res_blocks):
+            model += [ResidualBlock(dim)]
 
-        self.model_blocks = nn.Sequential(*layers)
-        self.shared_block = shared_block
-
-    def reparameterization(self, mu):
-        FloatTensor = torch.cuda.FloatTensor if mu.is_cuda else torch.FloatTensor
-        z = Variable(FloatTensor(np.random.normal(0, 1, mu.shape)))
-        return z + mu
-
-    def forward(self, x):
-        x = self.model_blocks(x)
-        mu = self.shared_block(x)
-        z = self.reparameterization(mu)
-        return mu, z
-
-
-class Generator(nn.Module):
-    def __init__(self, out_channels=3, dim=64, n_upsample=2, shared_block=None):
-        super(Generator, self).__init__()
-
-        layers = []
-        dim = dim * 2 ** n_upsample
-        self.shared_block = shared_block
-
-        # residual blocks
-        for _ in range(3):
-            layers += [ResidualBlock(dim)]
-
-        # Upsampling
-        for _ in range(n_upsample):
-            layers += [nn.ConvTranspose2d(dim, dim//2, 4, stride=2, padding=1),
-                       nn.InstanceNorm2d(dim//2),
-                       nn.LeakyReLU(0.2, inplace=True)]
+        # Usampling
+        for _ in range(2):
+            model += [nn.ConvTranspose2d(dim, dim // 2, 4, stride=2, padding=1, bias=False),
+                      nn.InstanceNorm2d(dim // 2, affine=True, track_running_stats=True),
+                      nn.ReLU(inplace=True)]
             dim = dim // 2
 
         # Output layer
-        layers += [nn.ReflectionPad2d(3),
-                   nn.Conv2d(dim, out_channels, 7),
-                   nn.Tanh()]
+        model += [nn.Conv2d(dim, channels, 7, stride=1, padding=3), nn.Tanh()]
+        self.model = nn.Sequential(*model)
 
-        self.model_blocks = nn.Sequential(*layers)
-        self.shared_block = shared_block
+    def forward(self, x, c):
+        c = c.view(c.size(0), c.size(1), 1, 1)
+        c = c.repeat(1, 1, x.size(2), x.size(3))
+        x = torch.cat((x, c), 1)
 
-    def forward(self, x):
-        x = self.shared_block(x)
-        x = self.model_blocks(x)
-        return x
+        return self.model(x)
 
 
 class Discriminator(nn.Module):
-    def __init__(self, input_shape):
+    def __init__(self, channel, img_size, c_dim=5, n_strided=6):
         super(Discriminator, self).__init__()
 
-        # Calculate output shape of image discriminator (PatchGAN)
-        channels, height, width = input_shape
-        self.output_shape = (1, height // 2 ** 4, width // 2 ** 4)
-
-        def discriminator_block(in_filters, out_filters, stride=2, normalize=True):
-            layers = [nn.Conv2d(in_filters, out_filters, 4, stride, 1)]
-            if normalize:
-                layers.append(nn.InstanceNorm2d(out_filters))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
+        def discriminator_block(in_filters, out_filters, stride=2):
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride, 1),
+                      nn.LeakyReLU(0.01, inplace=True)]
             return layers
 
-        self.model = nn.Sequential(*discriminator_block(channels, 64, normalize=False),
-                                   *discriminator_block(64, 128),
-                                   *discriminator_block(128, 256),
-                                   *discriminator_block(256, 512),
-                                   nn.Conv2d(512, 1, 3, padding=1))
+        dim = 64
+        kernel_size = img_size // (2 ** n_strided)
+
+        layers = discriminator_block(channel, dim)
+        for _ in range(n_strided - 1):
+            layers.extend(discriminator_block(dim, dim * 2))
+            dim *= 2
+
+        self.model = nn.Sequential(*layers)
+        self.out1 = nn.Conv2d(dim, 1, 3, padding=1, bias=False)
+        self.out2 = nn.Conv2d(dim, c_dim, kernel_size, bias=False)
 
     def forward(self, img):
-        return self.model(img)
+        feature = self.model(img)
+
+        out_adv = self.out1(feature)
+        out_cls = self.out2(feature)
+
+        return out_adv, out_cls.view(out_cls.size(0), -1)
