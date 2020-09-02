da855b42d76fe69095802295947dbe1d4c98c7b6
GodWriter <18917565247@163.com>
2020-09-02 15:33:22 +0800

    Commit StarGAN.

--- stargan/utils.py
+++ stargan/utils.py
@@ -14,7 +14,7 @@ label_changes = [((0, 1), (1, 0), (2, 0)),  # Set to black hair
                  ((0, 0), (1, 1), (2, 0)),  # Set to blonde hair
                  ((0, 0), (1, 0), (2, 1)),  # Set to brown hair
                  ((3, -1),),  # Flip gender
-                 ((4, -1),),  # Age flip
+                 ((4, -1),),  # Flip age
                  ]
 
 
@@ -57,8 +57,38 @@ def resize_img(path):
         img.save(img_path)
 
 
-def save_sample(dataset, test_loader, batches_done, E1, E2, G1, G2, FloatTensor):
-    pass
+def save_sample(dataset, val_loader, batches_done, generator, c_dim, FloatTensor):
+    """
+    Example: [1., 0., 0., 0., 0.]
+    Result:
+       [[1., 0., 0., 0., 0.],
+        [0., 1., 0., 0., 0.],
+        [0., 0., 1., 0., 0.],
+        [1., 0., 0., 1., 0.],
+        [1., 0., 0., 0., 1.]]
+    Note: To understand the following code, you can get help from the generated images
+    """
+
+    val_imgs, val_labels = next((iter(val_loader)))
+
+    val_imgs = Variable(val_imgs.type(FloatTensor))
+    val_labels = Variable(val_labels.type(FloatTensor))
+    img_samples = None
+
+    for i in range(10):
+        img, label = val_imgs[i], val_labels[i]
+        imgs = img.repeat(c_dim, 1, 1, 1)
+        labels = label.repeat(c_dim, 1)
+
+        for sample_i, changes in enumerate(label_changes):
+            for col, val in changes:
+                labels[sample_i, col] = 1 - labels[sample_i, col] if val == -1 else val
+
+        gen_imgs = generator(imgs, labels)
+        gen_imgs = torch.cat([x for x in gen_imgs.data], -1) # Concatenate images by width
+        img_sample = torch.cat((img.data, gen_imgs), -1)
+        img_samples = img_sample if img_samples is None else torch.cat((img_samples, img_sample), -2) # Add as row to generated samples
+
 
 
 if __name__ == "__main__":
